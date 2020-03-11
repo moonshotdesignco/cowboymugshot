aca15f5a73a605c8628db81f961df99d3bd03911
Will <moonshotdesignco@gmail.com>
2020-03-11 19:26:24 -0400

    background carousel

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 document.addEventListener("touchstart", function() {},false);
+
+function backGround() {
+  var n = Math.floor((Math.random() * 20) + 1);
+  document.getElementById("cmhtml").setAttribute("style", "background-image:url('../bg/" + n + ".jpg');");
+}
+
+backGround();
+
+setInterval(backGround, 6000);
