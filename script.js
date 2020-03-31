aafd5db3688e18f3d8b71355f8d8d36f3c7f0cf3
Will <moonshotdesignco@gmail.com>
2020-03-31 16:19:45 -0400

    tallyban + optimization

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("touchstart", function() {},false);
 
 function backGround() {
-  var n = Math.floor((Math.random() * 20) + 1);
+  var n = Math.floor((Math.random() * 10) + 1);
   document.getElementById("cmhtml").setAttribute("class", "bg" + n);
 }
 
 backGround();
 
-setInterval(backGround, 6000);
+setInterval(backGround, 7000);
